7c13f1f2322ea341ebcbd28fba6c46fe2b64a928
torabe <s2808282245@gmail.com>
2021-06-10 01:36:37 +0900

    エンドポイント修正

--- components/Task.js
+++ components/Task.js
@@ -8,7 +8,7 @@ const cookie = new Cookie();
 export default function Task({ task, taskDeleted }) {
   const { setSelectedTask } = useContext(StateContext);
   const deleteTask = async () => {
-    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}`, {
+    await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/tasks/${task.id}/`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json',
